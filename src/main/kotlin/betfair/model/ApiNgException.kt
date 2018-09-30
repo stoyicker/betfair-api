ecf58e41d940e5639d383d07620432246f337025
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2018-09-30 17:07:12 +0200

    Remove completed TODO

--- src/main/kotlin/betfair/model/ApiNgException.kt
+++ src/main/kotlin/betfair/model/ApiNgException.kt
@@ -2,7 +2,6 @@ package betfair.model
 
 import com.squareup.moshi.Json
 
-// TODO private and Json-annotate all of these fields
 internal data class ApiNgException(
 		@Json(name = "errorCode")
 		private val errorCode: ApiNgExceptionErrorCode? = null,
