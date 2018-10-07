bd8f2ea018e76932391166e1fda269ab16a6c9f0
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2018-10-07 09:13:33 +0200

    Expose betting methods via Retrofit interface

--- src/main/kotlin/betfair/BettingApiClient.kt
+++ src/main/kotlin/betfair/BettingApiClient.kt
@@ -3,6 +3,7 @@ package betfair
 import betfair.api.AcceptJsonInterceptor
 import betfair.api.ApplicationKeyInterceptor
 import betfair.api.AuthenticationInterceptor
+import betfair.api.BettingJsonApiService
 import betfair.api.BotLoginApiService
 import betfair.api.ContentTypeApplicationJsonInterceptor
 import betfair.api.HttpCompressionInterceptor
@@ -16,49 +17,47 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 
 // TODO Afterwards, configure the authentication client to use the certificate
-class BettingApiClient(
-		private val username: String,
-		private val password: String,
-		private val config: Config) {
-	private val botLoginService = OkHttpClient.Builder()
-			.connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
-			.readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
-			.writeTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
-			.addInterceptor(AcceptJsonInterceptor())
-			.addInterceptor(ApplicationKeyInterceptor(config.applicationKey))
-			.addInterceptor(HttpCompressionInterceptor())
-			.addInterceptor(HttpKeepAliveInterceptor())
-			.addInterceptor(HttpNoCacheInterceptor())
-			.build().run {
-				Retrofit.Builder()
-						.client(this)
-						.baseUrl(BotLoginApiService.ENDPOINT)
-						.addConverterFactory(MoshiConverterFactory.create())
-						.build()
-						.create(BotLoginApiService::class.java)
-			}
-	private val bettingApiClient: OkHttpClient = OkHttpClient.Builder()
-			.connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
-			.readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
-			.writeTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
-			.addInterceptor(AcceptJsonInterceptor())
-			.addInterceptor(ApplicationKeyInterceptor(config.applicationKey))
-			.addInterceptor(AuthenticationInterceptor(botLoginService, config))
-			.addInterceptor(ContentTypeApplicationJsonInterceptor())
-			.addInterceptor(HttpCompressionInterceptor())
-			.addInterceptor(HttpKeepAliveInterceptor())
-			.addInterceptor(HttpNoCacheInterceptor())
-			.build().apply {
-				Retrofit.Builder()
-						.client(this)
-						.baseUrl(BotLoginApiService.ENDPOINT)
-						.addConverterFactory(MoshiConverterFactory.create())
-						.addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
-						.build()
-						.create(BotLoginApiService::class.java)
-			}
-
+class BettingApiClient private constructor(bettingJsonService: BettingJsonApiService)
+	: BettingJsonApiService by bettingJsonService {
 	companion object {
 		private const val TIMEOUT_SECONDS = 30L
+		fun from(config: Config) = OkHttpClient.Builder()
+				.connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+				.readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+				.writeTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+				.addInterceptor(AcceptJsonInterceptor())
+				.addInterceptor(ApplicationKeyInterceptor(config.applicationKey))
+				.addInterceptor(AuthenticationInterceptor(
+						OkHttpClient.Builder()
+								.connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+								.readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+								.writeTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+								.addInterceptor(AcceptJsonInterceptor())
+								.addInterceptor(ApplicationKeyInterceptor(config.applicationKey))
+								.addInterceptor(HttpCompressionInterceptor())
+								.addInterceptor(HttpKeepAliveInterceptor())
+								.addInterceptor(HttpNoCacheInterceptor())
+								.build().run {
+									Retrofit.Builder()
+											.client(this)
+											.baseUrl(BotLoginApiService.ENDPOINT)
+											.addConverterFactory(MoshiConverterFactory.create())
+											.build()
+											.create(BotLoginApiService::class.java)
+								},
+						config))
+				.addInterceptor(ContentTypeApplicationJsonInterceptor())
+				.addInterceptor(HttpCompressionInterceptor())
+				.addInterceptor(HttpKeepAliveInterceptor())
+				.addInterceptor(HttpNoCacheInterceptor())
+				.build().apply {
+					Retrofit.Builder()
+							.client(this)
+							.baseUrl(BotLoginApiService.ENDPOINT)
+							.addConverterFactory(MoshiConverterFactory.create())
+							.addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+							.build()
+							.create(BotLoginApiService::class.java)
+				}
 	}
 }
